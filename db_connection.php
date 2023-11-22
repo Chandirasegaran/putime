8c893320714bd2536dc54a6760d4bddcbf489ca8
msMukesh <msmukesh2001@gmail.com>
2023-11-22 21:23:13 +0530

    my first git commit

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost"; // Replace with your MySQL server name
 $username = "root"; // Replace with your MySQL username
-$password = "2503"; // Replace with your MySQL password
+$password = "semEIGHT*8"; // Replace with your MySQL password
 $dbname = "pu_timetable"; // Replace with your database name
 
 // Create connection
