2774d465319abe25c4e35d5fbb9e574a43de913a
Chandirasegaran <chandirasegaransegar@gmail.com>
2023-11-24 20:32:06 +0530

    Added SQL Password

---
+++ Old Main/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; // Replace with your MySQL server name
+$username = "root"; // Replace with your MySQL username
+$password = "2503"; // Replace with your MySQL password
+$dbname = "pu_timetable"; // Replace with your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
