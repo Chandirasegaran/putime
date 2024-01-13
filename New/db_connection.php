bce13f71a5d5876af7bcf3a44f51c053a12c5fe7
chandirasegaran <chandirasegaransegar@gmail.com>
2024-01-13 23:13:28 +0530

    ESTABLISHED THE DB CONNECTION CREATED THE TABLE FOR EACH COURSE

--- New/db_connection.php
+++ New/db_connection.php
@@ -1,7 +1,11 @@
 <?php
-    // Database connection
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "putimetbdb";
-?>  
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "putimetbdb";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die('Connection Error: ' . $conn->connect_error);
+}
+?>
