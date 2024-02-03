7293ac2fad3e9b89a899f5662d61fffc94a51bb8
msMukesh <msmukesh2001@gmail.com>
2024-02-03 17:24:45 +0530

    conjainer seg

--- schedule.php
+++ schedule.php
@@ -13,11 +13,7 @@
 
 <body>
     <?php include 'navbar.php' ?>
-<<<<<<< HEAD
     <div class="container-fluid mt-5">
-=======
-    <div class="container mt-5">
->>>>>>> 1ce007d9d867ab5a66dc27265d95538e0dc37934
         <H1>Assign Schedule</H1>
         <div id="assign-schedule">
 
